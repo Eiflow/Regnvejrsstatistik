5e35b5960edc29abc8777c62264c9489b45390ba
Jesper <Jesper@DESKTOP-FCO0SM6>
2023-12-12 13:11:30 +0100

    Add project files.

---
+++ Regnvejrsstatistik/Program.cs
@@ -0,0 +1,126 @@
+﻿namespace Regnvejrsstatistik
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] ugentligNedbør = new double[7];
+            InitializeArray(ugentligNedbør);
+
+            int valg;
+            do
+            {
+                Console.WriteLine("\nVejr App Menu:");
+                Console.WriteLine("1. Indtast Nedbørsdata for Ugen");
+                Console.WriteLine("2. Beregn Statistik for en Bestemt Dag");
+                Console.WriteLine("3. Beregn Statistik for Hele Ugen");
+                Console.WriteLine("4. Afslut");
+                Console.Write("Indtast dit valg (1-4): ");
+
+                if (int.TryParse(Console.ReadLine(), out valg))
+                {
+                    switch (valg)
+                    {
+                        case 1:
+                            IndtastUgentligNedbør(ugentligNedbør);
+                            break;
+                        case 2:
+                            BeregnDagligStatistik(ugentligNedbør);
+                            break;
+                        case 3:
+                            BeregnUgeStatistik(ugentligNedbør);
+                            break;
+                        case 4:
+                            Console.WriteLine("Afslutter Vejr App. Farvel!");
+                            break;
+                        default:
+                            Console.WriteLine("Ugyldigt valg. Indtast venligst et nummer mellem 1 og 4.");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Ugyldig indtastning. Indtast venligst et gyldigt nummer.");
+                }
+
+            } while (valg != 4);
+        }
+
+        static void InitializeArray(double[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = 0;
+            }
+        }
+
+        static void IndtastUgentligNedbør(double[] ugentligNedbør)
+        {
+            string[] ugedage = { "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag" };
+
+            for (int dag = 0; dag < ugentligNedbør.Length; dag++)
+            {
+                Console.Write($"Indtast nedbør for {ugedage[dag]} (mm): ");
+                double nedbør;
+                while (!double.TryParse(Console.ReadLine(), out nedbør))
+                {
+                    Console.WriteLine("Ugyldig indtastning. Indtast venligst et gyldigt nummer.");
+                    Console.Write($"Indtast nedbør for {ugedage[dag]} (mm): ");
+                }
+
+                ugentligNedbør[dag] = nedbør;
+            }
+
+            Console.WriteLine("\nUgentlig nedbørsdata indtastet succesfuldt!");
+        }
+
+        static void BeregnDagligStatistik(double[] ugentligNedbør)
+        {
+            string[] ugedage = { "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag" };
+
+            Console.Write("Indtast dagen, hvor du vil beregne statistik: ");
+            string valgtDag = Console.ReadLine();
+
+            int dagIndex = Array.IndexOf(ugedage, valgtDag);
+            if (dagIndex != -1)
+            {
+                Console.WriteLine($"\nStatistik for {valgtDag}:");
+                Console.WriteLine($"Nedbør: {ugentligNedbør[dagIndex]} mm");
+            }
+            else
+            {
+                Console.WriteLine("Ugyldig dag. Indtast venligst en gyldig ugedag.");
+            }
+        }
+
+        static void BeregnUgeStatistik(double[] ugentligNedbør)
+        {
+            double samletNedbør = 0;
+            double maksNedbør = double.MinValue;
+            double minNedbør = double.MaxValue;
+
+            for (int dag = 0; dag < ugentligNedbør.Length; dag++)
+            {
+                double nedbør = ugentligNedbør[dag];
+                samletNedbør += nedbør;
+
+                if (nedbør > maksNedbør)
+                {
+                    maksNedbør = nedbør;
+                }
+
+                if (nedbør < minNedbør)
+                {
+                    minNedbør = nedbør;
+                }
+            }
+
+            double gennemsnitligNedbør = samletNedbør / ugentligNedbør.Length;
+
+            Console.WriteLine("\nUgestatistik:");
+            Console.WriteLine($"Gennemsnitlig Nedbør: {gennemsnitligNedbør:F2} mm");
+            Console.WriteLine($"Maksimal Nedbør på en Dag: {maksNedbør} mm");
+            Console.WriteLine($"Minimal Nedbør på en Dag: {minNedbør} mm");
+        }
+    }
+}
---
+++ Regnvejrsstatistik/readme.md
@@ -0,0 +1,41 @@
+# Vejrstatistik Applikation
+
+Vejrstatistik Applikationen er en konsolapplikation, der giver dig mulighed for at indtaste og analysere ugentlige nedbørsdata og beregne statistik for forskellige dage i ugen.
+
+## Brugervejledning
+
+### 1. Kør Programmet
+
+For at køre programmet kræves et C#-udviklingsmiljø.
+
+1. Download (`Vejrstatistik.cs`).
+2. Kompiler og kør programmet ved hjælp af en C#-kompiler Visual Studio Code.
+
+### 2. Menuvalg
+
+Programmet præsenterer følgende menuvalg:
+
+- **Indtast Nedbørsdata for Ugen (1):** Indtast den samlede nedbør for hver dag i ugen.
+- **Beregn Statistik for en Bestemt Dag (2):** Beregn og vis statistik for en specifik dag baseret på indtastede nedbørsdata.
+- **Beregn Statistik for Hele Ugen (3):** Beregn og vis statistik for hele ugen baseret på indtastede nedbørsdata.
+- **Afslut (4):** Luk programmet.
+
+### 3. Indtastning af Nedbørsdata
+
+Vælg mulighed 1 for at indtaste den samlede nedbør for hver dag i ugen. Følg anvisningerne for at indtaste data.
+
+### 4. Beregning af Statistik for en Bestemt Dag
+
+Vælg mulighed 2 for at beregne og vise statistik for en specifik dag. Indtast dagen, når du bliver bedt om det.
+
+### 5. Beregning af Statistik for Hele Ugen
+
+Vælg mulighed 3 for at beregne og vise statistik for hele ugen baseret på indtastede nedbørsdata.
+
+### 6. Afslutning af Programmet
+
+Vælg mulighed 4 for at afslutte programmet.
+
+## Bemærkninger
+
+- Ugedage repræsenteres ved deres navne – Mandag, Tirsdag og så videre.
